690b55522fafe46b321d8f05d59dc0d0bb0356ae
nounrr <sosi66581@gmail.com>
2025-10-31 01:22:26 +0100

    Update CORS configuration to allow additional origins for API access

--- sirh-back/config/cors.php
+++ sirh-back/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'public/api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173','http://localhost:5174', 'http://127.0.0.1:5174'],
+    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173','http://localhost:5174', 'http://127.0.0.1:5174','http://dct.smartrh.ma','http://dct.smartrh.ma/public','*'],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
 ];
